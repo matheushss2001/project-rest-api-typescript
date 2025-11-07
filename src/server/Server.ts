15054b3a713f2889b3b4f2e74a62b4560fe7466a
Matheus Silva <theushsantos16@gmail.com>
2025-11-06 21:33:43 -0300

    project updating

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import { router } from './routes';
+import 'dotenv/config';
 
 
 const server = express();
 
+server.use(express.json());
 
-server.get('/', (_, res) => {
-    return res.send('Olá, DEV!');
-})
+server.use(router);
+
 
 
 export { server };
---
+++ src/server/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const router = Router();// instanciando o objeto do Tipo Router (rota)
+
+// criando uma nova toda com a função get
+router.get('/', (_, res) => { // criou o endpoint
+    return res.send("Olá, DEV!");
+});
+
+router.post('/teste',(req,res) => { /// criou uma nova rota
+    console.log(req);
+
+    return res.status(StatusCodes.UNAUTHORIZED).json(req.body)
+})
+
+export { router };
